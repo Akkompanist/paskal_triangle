6c12f72bd7017be5132ba31f894759be893e3c8c
Accompanist <96627800+Akkompanist@users.noreply.github.com>
2022-04-29 20:56:23 +0300

    Add files via upload

---
+++ paskal_triangle.py
@@ -0,0 +1,29 @@
+
+import tkinter as tk
+
+def paskal_triangle(old_pt):
+    pt = []
+    for i in range(len(old_pt)):
+        pt.append(old_pt[i-1] + old_pt[i]) if i != 0 else pt.append(old_pt[i])
+    pt.append(1)
+    return pt
+
+root = tk.Tk()
+root.geometry("1200x900")
+T1 = tk.Text(root, height=900, width=1200, font=("Helvetica", 5))
+T1.tag_configure("center", justify='center')
+
+
+start = [1]
+
+
+for i in range(101):
+    pt = paskal_triangle(start)
+    T1.insert("1.0", start)
+    T1.insert("1.0", "\n")
+    start = pt
+
+T1.tag_add("center", "1.0", "end")
+T1.pack()
+
+root.mainloop()
